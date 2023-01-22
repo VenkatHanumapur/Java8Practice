1d1c6cbecece72bb168e4df8fcc0ae1df9ee2201
venkathanumapur <venkat.hanumapur@gmail.com>
2023-01-22 16:47:10 -0600

    updated main class

--- Java8Practice/src/com/section2/LambdPractice2.java
+++ Java8Practice/src/com/section2/LambdPractice2.java
@@ -5,6 +5,9 @@ public class LambdPractice2 {
 	public static void main(String[] args) {
 		MathOperation sum = (a,b) -> System.out.println(a+b);
 		sum.add(29, 59);
+		
+		MathOperation multiply = (a,b) -> System.out.println(a*b);
+		multiply.add(78, 90);
 
 	}
 
