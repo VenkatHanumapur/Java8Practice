20c54aa8e451179ad5cac0ad8fb95dcd0f019b52
venkathanumapur <venkat.hanumapur@gmail.com>
2023-01-22 16:33:55 -0600

    //No return type and no parameter

---
+++ Java8Practice/src/com/section2/LamdaPractice1.java
@@ -0,0 +1,13 @@
+package com.section2;
+
+
+
+public class LamdaPractice1 {
+
+	public static void main(String[] args) {
+		Name name= () -> System.out.println("John");
+		name.myName();
+
+	}
+
+}
---
+++ Java8Practice/src/com/section2/Name.java
@@ -0,0 +1,8 @@
+package com.section2;
+
+//No return type and no parameter
+@FunctionalInterface
+public interface Name {	
+	void myName();
+
+}
